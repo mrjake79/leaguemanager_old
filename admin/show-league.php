5144dcd3fb8f48a7ceced73a95a1c647f88e454c
Jake Bahnsen <mrjake79@gmail.com>
2014-12-06 09:34:14 -0700

    Strip slashes so team names and locations do not end up with them stored in the database.

--- admin/show-league.php
+++ admin/show-league.php
@@ -1,7 +1,10 @@
 <?php
 if ( isset($_POST['updateLeague']) && !isset($_POST['doaction']) && !isset($_POST['doaction2']) && !isset($_POST['doaction3']) )  {
 	if ( 'team' == $_POST['updateLeague'] ) {
-		check_admin_referer('leaguemanager_manage-teams');
+        check_admin_referer('leaguemanager_manage-teams');
+
+        $_POST = stripslashes_deep($_POST);
+
 		$home = isset( $_POST['home'] ) ? 1 : 0;
 		$custom = !isset($_POST['custom']) ? array() : $_POST['custom'];
 		$roster = ( isset($_POST['roster_group']) && isset($_POST['roster']) ) ? array('id' => $_POST['roster'], 'cat_id' => $_POST['roster_group']) : array( 'id' => '', 'cat_id' => false );
